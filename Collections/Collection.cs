6f36c8fc5a2e408addb5be1534818d0b40ef9b1b
dimosoftuni <104933623+dimosoftuni@users.noreply.github.com>
2022-10-16 09:23:59 +0300

    Update the initial capacity

--- Collections/Collection.cs
+++ Collections/Collection.cs
@@ -5,7 +5,7 @@ namespace Collections
 {
     public class Collection<T>
     {
-        private const int InitialCapacity = 16;
+        private const int InitialCapacity = 4;
         private T[] items;
         public int Capacity => this.items.Length;
         public int Count { get; private set; }
